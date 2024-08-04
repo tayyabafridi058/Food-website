98b83916f43cf220fa5495e12941f4f45559d74c
TAYYAB AFRIDI <tayyabafridi058>
2024-08-04 01:26:00 -0700

    section complete

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,7 +4,9 @@ function Footer() {
   return (
     <footer>
         <div className="social">
-
+            <img src="../src/assest/facebook.png" alt="" />
+            <img src="../src/assest/instagram.png" alt="" />
+            <img src="../src/assest/twitter.png" alt="" />
         </div>
         <p>&copy;Copyright by Tayyab Afridi</p>
     </footer>
