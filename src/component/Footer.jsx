d522c4841449879563ba4c7c43fca1875b9bc20e
TAYYAB AFRIDI <tayyabafridi058>
2024-08-04 01:30:20 -0700

    social link add

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer>
         <div className="social">
             <img src="../src/assest/facebook.png" alt="" />
-            <img src="../src/assest/instagram.png" alt="" />
+            <img src="../src/assest/linkedin.png" alt="" />
             <img src="../src/assest/twitter.png" alt="" />
         </div>
         <p>&copy;Copyright by Tayyab Afridi</p>
